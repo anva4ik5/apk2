8a5fbb18cf4fe2a95605cf3d1e4db029c4096289
anva4ik5 <copilottt707@gmail.com>
2026-04-16 04:14:01 +0500

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -68,7 +68,7 @@ CREATE TABLE IF NOT EXISTS sessions (
     device_name VARCHAR(100),
     device_type VARCHAR(50), -- 'mobile', 'desktop', 'web'
     device_os VARCHAR(50),
-    ip_address IPv4,
+    ip_address INET,
     user_agent TEXT,
     
     -- Session State
@@ -355,7 +355,7 @@ CREATE TABLE IF NOT EXISTS audit_log (
     -- Event Info
     event_type VARCHAR(100) NOT NULL,
     event_data JSONB,  -- Flexible event data
-    ip_address IPv4,
+    ip_address INET,
     user_agent TEXT,
     
     -- Metadata
